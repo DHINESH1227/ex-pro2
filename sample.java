b9c2660812706502fbd8d20111be4f4168be630c
DHINESH1227 <24203010@srcas.ac.in>
2025-07-14 11:20:01 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,1 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
 
+class Geeks
+{
+
+    public static void main(String[] args)
+    {
+     
+        int num;
+
+     
+        System.out.println("Enter the integer: ");
+
+  
+        Scanner s = new Scanner(System.in);
+
+       
+        num = s.nextInt();
+
+       
+        System.out.println("Entered integer is: " + num);
+
+        
+        s.close(); 
+    }
+}
